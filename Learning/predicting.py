4249d9b2fadc98727a5b2b4b4e98e49d0b081363
Keyang Xu <xky0714@126.com>
2015-06-20 13:36:49 +0800

    6.20

--- Learning/predicting.py
+++ Learning/predicting.py
@@ -239,7 +239,8 @@ if __name__ == "__main__":
 	vertical_features_5 = get_features("vertical_features_5.txt")
 	vertical_features = get_features("vertical_features.txt")
 
-	#position_features = 
+	position_features = get_features("position_features.txt")
+	position_features_5 = get_features("position_features_5.txt")
 
 	url_features_5 = get_features("url_features_5.txt")
 	url_features = get_features("url_features.txt")
@@ -247,10 +248,10 @@ if __name__ == "__main__":
 	
 	
 
-	position_features_sogou = get_features("Sogou_position.txt")
-	position_features_baidu = get_features ("Baidu_position.txt")
+	#position_features_sogou = get_features("Sogou_position.txt")
+	#position_features_baidu = get_features ("Baidu_position.txt")
 
-	aspects_1 = [query_features,text_features_5,url_features_5]
+	aspects_1 = [position_features_5,vertical_features_5,query_features,text_features_5]
 	aspects_2 = [vertical_features_5]
 	#aspects = [url_features]
 	#y = ground_truth("../Result/result.txt","difference","contradiction",1000)
@@ -271,20 +272,80 @@ if __name__ == "__main__":
 	print "we have " + str(y1) + "for 1 and " + str(y0) + " for 0" 
 	clf = GradientBoostingClassifier(n_estimators=47, learning_rate=0.03,max_depth=3,random_state=0)
 	test_X = clf.fit_transform(X,y)
+	#clf.fit(X,y)
 	importances  = clf.feature_importances_
-	indices = np.argsort(importances)[::-1]
-	for f in range(10):
-		print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+	position_propotion = 0.0 # 0-8
+	vertical_propotion = 0.0 # 9-74
+	query_propotion  =0.0 #75-77
+	text_propotion = 0.0 #78 - last 
+	#print "size of importances " + str(len(importances))
+
+	#indices = np.argsort(importances)[::-1]
+	#for f in range(10):
+	#	print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
 	
-	print len(test_X[0])
+	#print len(test_X[0])
 	#clf2 = svm.SVC(C=1.0, kernel='rbf', degree=3, gamma=0.0, coef0=0.0, shrinking=True, probability=False, tol=0.001, cache_size=200, class_weight=None, verbose=False, max_iter=-1, random_state=None)
 	test = ['accuracy','recall_macro','f1_macro','roc_auc']
 	#test =  ['accuracy','f1_macro']
 	clf2 = GradientBoostingClassifier(n_estimators=47, learning_rate=0.03,max_depth=3,random_state=0)
-	for item in test:
-		scores = cross_validation.cross_val_score(clf2,X,y,cv=5,scoring=item)
-		print(item+" score is "+str(sum(scores)/5))
+	#for item in test:
+	#	scores = cross_validation.cross_val_score(clf2,X,y,cv=5,scoring=item)
+	#	print(item+" score is "+str(sum(scores)/5))
+	gap = 77
+	start_ = 0
+	end_ = len(X)
+	y_predict = {}
+	y_prob = {}
+	number = 0
+	count = 0
+	for i in range(5):
+		start = i*gap
+		end = (i+1)*gap
+		X_test, y_test = X[start:min(end,end_)],y[start:min(end,end_)]
+		X_train = X[start_:start] + X[end:end_]
+		y_train  = y[start_:start] + y[end:end_]
+		print len(y_test)
+		clf2.fit(X_train,y_train)
+		temp =  clf2.predict_proba(X_test)
+		print len(temp)
+		for item in temp:
+			if item[0] <= item[1]:
+				y_predict[number] = 1
+				y_prob[number] = item[1]
+			else:
+				y_predict[number] = 0
+				y_prob[number] = item[0]
+			number += 1
 
+	print len(y_prob)
+	print len(y_predict)
+	for i in range(end_):
+		if y_predict[i] == y[i]:
+			count += 1
+	print float(count)/float(end_)
+
+	sorted_prob_list= sorted(y_prob.iteritems(), key=lambda d:d[1], reverse = True)
+	#for item in sorted_prob_list:
+	#	print str(item[0]) + "\t" + str(item[1])
+	#print y_predict
+	count_right = 0
+	right_dict = {}
+	for i in range(end_):
+		key = sorted_prob_list[i][0]
+		print str(key) + "\t" + str(y_predict[key]) + "\t" + str(y[key])
+		if y_predict[key] == y[key]:
+			count_right+=1 
+		right_dict[i+1] = count_right
+	test_point = [ i*38 for i in range(1,11)]
+	for item in test_point:
+		print float(right_dict[item])/float(item)
+
+
+
+
+
+'''
 	print "Majority Voting"
 	if y1 > y0:
 		y_major = [ 1 for i in range(len(y)) ]
@@ -297,7 +358,7 @@ if __name__ == "__main__":
 	precision = float(max(y1,y0))/float(len(y_major))
 	score = 2.0/(1/precision+2.0)
 	print "f1_macro score is " + str(score)
-	print "roc_auc score is " + str(roc_auc_score(y, y_major))
+	print "roc_auc score is " + str(roc_auc_score(y, y_major))'''
 '''
 	#transformX = clf.fit_transform(X[:size_X*4/5],y[:size_X*4/5])
 	#newx = clf.transform((X,threshold=0.01),y)
